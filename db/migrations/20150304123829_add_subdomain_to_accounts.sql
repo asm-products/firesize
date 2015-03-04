acd264ab5d444b0aa4ca526680483e30d3ced04e
vanstee <patrick@vanstee.me>
2015-03-04 16:13:15 -0500

    Fill in subdomains before requiring a unqiue field

--- db/migrations/20150304123829_add_subdomain_to_accounts.sql
+++ db/migrations/20150304123829_add_subdomain_to_accounts.sql
@@ -1,5 +1,6 @@
 -- +goose Up
 ALTER TABLE accounts ADD COLUMN subdomain text;
+UPDATE account SET subdomain = (SELECT substring(md5(random()::text) from 0 for 13));
 ALTER TABLE accounts ALTER COLUMN subdomain SET NOT NULL;
 CREATE UNIQUE INDEX subdomain_index ON accounts (subdomain);
 

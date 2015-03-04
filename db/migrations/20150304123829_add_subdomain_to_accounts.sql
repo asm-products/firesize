db0ea1b364a7c3f81b1f5730d989036028706a73
vanstee <patrick@vanstee.me>
2015-03-04 16:21:50 -0500

    Just add the row and make it unique in a later migration

--- db/migrations/20150304123829_add_subdomain_to_accounts.sql
+++ db/migrations/20150304123829_add_subdomain_to_accounts.sql
@@ -1,8 +1,5 @@
 -- +goose Up
 ALTER TABLE accounts ADD COLUMN subdomain text;
-UPDATE account SET subdomain = (SELECT substring(md5(random()::text) from 0 for 13));
-ALTER TABLE accounts ALTER COLUMN subdomain SET NOT NULL;
-CREATE UNIQUE INDEX subdomain_index ON accounts (subdomain);
 
 -- +goose Down
 ALTER TABLE accounts DROP COLUMN subdomain;

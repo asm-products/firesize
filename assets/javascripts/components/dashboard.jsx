4fa199929147d9ea3efe4f3d73dbec2097af9590
vanstee <patrick@vanstee.me>
2015-03-09 15:50:11 -0400

    Include some extras in the dashboard

--- assets/javascripts/components/dashboard.jsx
+++ assets/javascripts/components/dashboard.jsx
@@ -28,24 +28,53 @@ var Dashboard = React.createClass({
 
           <div className="bg-white border p3 mt3">
             <h2 className="mt0">Usage for March</h2>
-            <p className="mb0">You've used <strong>86/100</strong> of your <strong>Test</strong> plan this month.</p>
+            <p className="mb2 h3">You've used <strong>86/100</strong> of your <strong>Test</strong> plan this month.</p>
+
+            <h4 className="h6 gray bold caps">Recent Requests</h4>
+            <div className="overflow-scroll">
+              <table className="table-light mxn2 h6">
+                <thead>
+                  <tr>
+                    <th>Request</th>
+                    <th>Requested At</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <td>https://assembly.firesize.com/500x300/g_center/http://imgur.com/afhj348fh4</td>
+                    <td>03/12/2015 12:05PM EST</td>
+                  </tr>
+                  <tr>
+                    <td>https://assembly.firesize.com/500x300/g_center/http://imgur.com/afhj348fh4</td>
+                    <td>03/12/2015 12:05PM EST</td>
+                  </tr>
+                  <tr>
+                    <td>https://assembly.firesize.com/500x300/g_center/http://imgur.com/afhj348fh4</td>
+                    <td>03/12/2015 12:05PM EST</td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
           </div>
 
-          <div className="bg-white border p3 mt3">
-            <h2 className="mt0">Config</h2>
+          <div className="bg-white border p3 mt3 clearfix">
+            <h2 className="mt0">Configuration</h2>
 
-            <form>
-              <div>
-                <label>Subdomain</label>
-                <input type="text" value="assembly.firesize.com" />
-              </div>
+            <form className="col sm-col-6">
+              <fieldset className="fieldset-reset">
+                <div className="mb2">
+                  <label className="h6 gray bold caps">Subdomain</label>
+                  <input type="text" className="block full-width field-light" value="assembly.firesize.com" />
+                </div>
 
-              <div>
-                <label>Require all requests to be signed</label>
-                <input type="checkbox" />
-              </div>
+                <div className="mb2">
+                  <label className="h6 gray bold caps block full-width">Request Signing</label>
+                  <input type="checkbox" className="field-light mr1" style={{ height: '1rem' }} />
+                  <span className="mt2">Require all requests to be signed</span>
+                </div>
+              </fieldset>
 
-              <input type="submit" value="Save" className="mb0" />
+              <button type="submit" className="button">Save</button>
             </form>
           </div>
         </div>

dfdf40090655859dd94b63423c7faa6e8bd83cb0
vanstee <patrick@vanstee.me>
2015-03-12 13:47:16 -0400

    Allow users to update their account

--- controllers/accounts_controller.go
+++ controllers/accounts_controller.go
@@ -1,6 +1,7 @@
 package controllers
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 
@@ -13,18 +14,54 @@ type AccountsController struct {
 
 func (c *AccountsController) Init(r *mux.Router) {
 	r.HandleFunc("/api/account", c.Show).Methods("GET")
+	r.HandleFunc("/api/account", c.Update).Methods("PATCH")
+}
+
+type UpdateAccountParams struct {
+	Subdomain string `json:"subdomain"`
+	Plan      string `json:"plan"`
 }
 
 func (c *AccountsController) Show(w http.ResponseWriter, r *http.Request) {
-  authHeader := r.Header.Get("Authorization")
+	authHeader := r.Header.Get("Authorization")
 
-  account := models.FindAccountByJwt(authHeader)
-  if account == nil {
+	account := models.FindAccountByJwt(authHeader)
+	if account == nil {
 		http.Error(w, "Account not found", http.StatusUnauthorized)
 		return
-  }
+	}
 
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
-  fmt.Fprint(w, Response(account.Serialize()))
+	fmt.Fprint(w, Response(account.Serialize()))
+}
+
+func (c *AccountsController) Update(w http.ResponseWriter, r *http.Request) {
+	authHeader := r.Header.Get("Authorization")
+
+	account := models.FindAccountByJwt(authHeader)
+	if account == nil {
+		http.Error(w, "Account not found", http.StatusUnauthorized)
+		return
+	}
+
+	decoder := json.NewDecoder(r.Body)
+	var p UpdateAccountParams
+	err := decoder.Decode(&p)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	account.Subdomain = p.Subdomain
+	account.Plan = p.Plan
+	_, err = models.Update(account)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprint(w, Response(account.Serialize()))
 }

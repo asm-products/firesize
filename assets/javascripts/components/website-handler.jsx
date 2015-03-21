85631df54620dca2607156225d75f83bd23d2c17
vanstee <patrick@vanstee.me>
2015-03-21 12:51:51 -0400

    Free beta

--- assets/javascripts/components/website-handler.jsx
+++ assets/javascripts/components/website-handler.jsx
@@ -12,8 +12,14 @@ var WebsiteHandler = React.createClass({
   navbarLinks: function() {
     return (
       <a href="https://addons.heroku.com/firesize" className="red-dark">
-        <span className="sm-hide">Install</span>
-        <span className="sm-show">Install the Heroku add-on</span>
+        <span className="sm-hide">
+          <span className="inline-block bold mr2 py1 px2 white bg-blue rounded h6">Free during Beta!</span>
+          Install
+        </span>
+        <span className="sm-show">
+          <span className="inline-block bold mr2 py1 px2 white bg-blue rounded h6">Free during Beta!</span>
+          Install the Heroku add-on
+        </span>
       </a>
     );
   },

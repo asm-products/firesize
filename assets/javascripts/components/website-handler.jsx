114a1c13c0ca2f4f8306d046e8af4cee823f304c
vanstee <patrick@vanstee.me>
2015-03-24 16:48:34 -0400

    Fix nav and home page for signups

--- assets/javascripts/components/website-handler.jsx
+++ assets/javascripts/components/website-handler.jsx
@@ -11,17 +11,9 @@ var WebsiteHandler = React.createClass({
 
   navbarLinks: function() {
     return [
-      <a href="https://addons.heroku.com/firesize" className="red-dark">
-        <span className="sm-hide">
-          <span className="inline-block bold mr2 py1 px2 white bg-blue rounded h6">Free during Beta!</span>
-          Install
-        </span>
-        <span className="sm-show">
-          <span className="inline-block bold mr2 py1 px2 white bg-blue rounded h6">Free during Beta!</span>
-          Install the Heroku add-on
-        </span>
-      </a>,
-      <Link to="signin" className="red-dark ml3">Sign in</Link>
+      <a href="https://addons.heroku.com/firesize" className="red-dark">Install the Heroku add-on</a>,
+      <Link to="signup" className="button ml3">Sign up</Link>,
+      <Link to="signin" className="red-dark ml3">Sign in</Link>,
     ];
   },
 
--- assets/javascripts/entry.jsx
+++ assets/javascripts/entry.jsx
@@ -13,12 +13,12 @@ var WebsiteHandler = require('./components/website-handler.jsx');
 var routes = [
   <Route handler={WebsiteHandler}>
     <DefaultRoute name="home" handler={require('./components/home.jsx')} />
+    <Route name="signup" handler={require('./components/signup.jsx')} />
+    <Route name="signin" handler={require('./components/signin.jsx')} />
   </Route>,
   <Route handler={AppHandler}>
     <Route name="dashboard" handler={require('./components/dashboard.jsx')} />
     <Route name="docs" handler={require('./components/documentation.jsx')} />
-    <Route name="signup" handler={require('./components/signup.jsx')} />
-    <Route name="signin" handler={require('./components/signin.jsx')} />
     <Route name="signout" handler={require('./components/signout.jsx')} />
     <NotFoundRoute name="notfound" handler={require('./components/404.jsx')} />
   </Route>

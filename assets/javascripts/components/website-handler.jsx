702be5e8bc542f4ced2dfec811b48f633b54c03d
vanstee <patrick@vanstee.me>
2015-03-26 12:17:08 -0400

    v2 banner

--- assets/javascripts/components/website-handler.jsx
+++ assets/javascripts/components/website-handler.jsx
@@ -18,8 +18,22 @@ var WebsiteHandler = React.createClass({
   },
 
   render: function() {
+    var callout
+
+    if (window.location.pathname == "/") {
+      callout = (
+        <div className="bg-white shadow-inset">
+          <div className="container p3">
+            <div className="h3 mb1">D'oh. We messed up. We had a great launch, but we learned one important lesson.</div>
+            <span style={{ backgroundColor: '#FFF19B' }}>So we fixed it with a v2!</span> <a href="https://medium.com/@vanstee/and-that-s-why-you-should-always-ask-for-an-email-address-6a2deecd2d46" className="mb0">Read more about what went wrong, and how we solved the problem on our blog.</a>
+          </div>
+        </div>
+      )
+    }
+
     return (
       <div>
+        {callout}
         <Navbar links={this.navbarLinks()} />
         <RouteHandler />
       </div>
